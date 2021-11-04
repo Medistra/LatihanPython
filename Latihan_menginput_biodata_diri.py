b8dd321dca3fdfd341cd6b5e7ba600508aa13af2
Medistra <medistra37@gmail.com>
2021-11-04 00:52:15 -0700

    menambahkan file

---
+++ Latihan_menginput_biodata_diri.py
@@ -0,0 +1,13 @@
+print("Menginput Biodata Diri")
+
+name=input("nama:")
+call=input("call:")
+nim=input("nim:")
+address=input("address:")
+age=input("age:")
+dob=input("dob:")
+handphone=input("handphone:")
+
+print("Assalamualaikum, hello everyone")
+print("let me introduce my self. my name is",name,"but you can call me",call,"my NIM is",nim,"i was born in",dob,"and i ame",age,"years old. i am very glad if you want to invite my house in",address, ". so, don't forget to call me before with the handphone",handphone,"")
+print("Thank you")
